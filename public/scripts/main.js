af66082e53ffafe45fd5cb91c6b477ea15377564
aidan <67598470+livxy@users.noreply.github.com>
2022-11-15 11:21:54 -0500

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,39 +1,16 @@
-const canvas = document.getElementById('canv');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const ctx = canvas.getContext('2d');
-let cols = Math.floor(window.innerWidth / 20) + 1;
-let ypos = Array(cols).fill(0);
-
-ctx.fillStyle = '#000';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-function matrix () {
-  const w = window.innerWidth;
-  const h = window.innerHeight;
-  
-  if (canvas.width !== w) {
-    canvas.width = w;
-    cols = Math.floor(window.innerWidth / 20) + 1;
-    ypos = Array(cols).fill(0);
-  }
-  if (canvas.height !== h) {
-    canvas.height = h;
-  }
-
-  ctx.fillStyle = '#0001';
-  ctx.fillRect(0, 0, w, h);
-
-  ctx.fillStyle = '#0f0';
-  ctx.font = '15pt monospace';
-
-  ypos.forEach((y, ind) => {
-    const text = String.fromCharCode(Math.random() * 128);
-    const x = ind * 20;
-    ctx.fillText(text, x, y);
-    if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
-    else ypos[ind] = y + 20;
-  });
+var base = document.getElementById("title").innerHTML;
+var arr = [base];
+var sw = false;
+var len = 1, cur = 0;
+while (base.length > 2) {
+    base = base.slice(1, base.length - 1);
+    arr[len++] = base;
 }
-
-setInterval(matrix, 50);
+function upd() {
+    if (cur == 0 || cur == len - 1) sw = !sw;
+    if (sw) cur++; else cur--;
+    document.getElementById("title").innerHTML = arr[cur];
+    document.title = "- " + arr[cur] + " -";
+    setTimeout(upd, 500);
+}
+setTimeout(upd, 1000);
